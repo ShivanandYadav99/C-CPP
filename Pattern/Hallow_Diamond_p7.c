fb7e331813423fc463a3a7beb2abe04159cd6358
Shivanand Yadav <111229426+ShivanandYadav99@users.noreply.github.com>
2022-12-01 01:15:25 +0530

    Pattern Q7

---
+++ Pattern/Hallow_Diamond_p7.c
@@ -0,0 +1,52 @@
+#include<stdio.h>
+void main()
+{
+    int row,n,star,space,x=1,y;
+    printf("enter the number of row");
+    scanf("%d",&n);
+    for(row=1;row<=n;row++)
+    {
+        for(space=1;space<=n-row;space++)
+        printf("*");
+        for(star=1;star<=x;star++)
+        printf(" ");
+         for(space=1;space<=n-row;space++)
+        printf("*");
+        printf("\n");
+        x=x+2;
+    }
+    y=2*n-3;
+    for(row=1;row<=n-1;row++)
+    {
+        for(space=1;space<=row;space++)
+        printf("*");
+        for(star=1;star<=y;star++)
+        printf(" ");
+         for(space=1;space<=row;space++)
+        printf("*");
+        printf("\n");
+        y=y-2;
+    }
+
+}
+
+/*
+
+
+**** ****
+***   ***
+**     **
+*       *
+
+*       *
+**     **
+***   ***
+**** ****
+
+
+
+
+
+
+
+*/
